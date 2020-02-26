5d094975f47cd3d1ace351f0c234190c120abdca
Richell Bonfield <richell.bonfield12@gmail.com>
2020-02-26 19:24:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
   return driver;
 }
+
+function deleteFromDriverByKey(driver, key) {
+  const newObj = Object.assign({}, driver);
+  delete newObj[key];
+
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+
+  return driver;
+}
